1a548e0d58cb8e3e425963d765a05e791623787e
Marcos Amorim <markstm10@gmail.com>
2021-07-30 00:04:57 -0300

    🛠 Criado componente header e estilizado botão de nova transação

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import styled from "styled-components";
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   );
 }

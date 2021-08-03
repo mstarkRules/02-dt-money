57818677997956958a0900db601cc33597de75bc
Marcos Amorim <markstm10@gmail.com>
2021-08-02 22:53:08 -0300

    ❗ install types react-modal e criado funções pra abrir e fechar o modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
+import Modal from "react-modal";
+
 import logoImg from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
 
 export function Header() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={handleOpenNewTransactionModal}>
+          Nova transação
+        </button>
       </Content>
     </Container>
   );

c88632b9f18cbf1d1358b248f830236b7c918d0c
Marcos Amorim <markstm10@gmail.com>
2021-08-06 01:20:50 -0300

    🛠 criado botão de fechar e estilizado. Criado estilo do formulario e inputs

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,6 @@
 import Modal from "react-modal";
 import { Container } from "./styles";
+import closeImg from "../../assets/close.svg";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -17,6 +18,13 @@ export function NewTransactionModal({
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar modal" />
+      </button>
       <Container>
         <h2>Cadastrar Transação</h2>
         <input placeholder="Título" />

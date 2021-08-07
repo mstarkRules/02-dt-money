9c419720220cc0480abaa75661a6a7d5b67a31bd
Marcos Amorim <markstm10@gmail.com>
2021-08-06 21:37:47 -0300

    ✨ criado e estilizado botões de entrada e saída

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from "react-modal";
-import { Container } from "./styles";
+import { Container, TransactionTypeContainer } from "./styles";
 import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -29,8 +31,18 @@ export function NewTransactionModal({
         <h2>Cadastrar Transação</h2>
         <input placeholder="Título" />
         <input type="number" placeholder="Valor" />
-        <input placeholder="Categoria" />
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
 
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+        <input placeholder="Categoria" />
         <button type="submit">Cadastrar</button>
       </Container>
     </Modal>

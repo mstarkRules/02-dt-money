b327dbd0f5754dcd0511b6d3db1ef78223bd7d32
Marcos Amorim <markstm10@gmail.com>
2021-08-12 23:54:23 -0300

    🛠 buscado dados pre cadastrados e listados no transaction table

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,29 @@ createServer({
   models: {
     transaction: Model,
   },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freela de website",
+          type: "deposit",
+          category: "Dev",
+          amount: 7000,
+          createdAt: new Date("2021-08-05 18:00:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1500,
+          createdAt: new Date("2021-08-04 12:00:00"),
+        },
+      ],
+    });
+  },
   routes() {
     this.namespace = "api";
 

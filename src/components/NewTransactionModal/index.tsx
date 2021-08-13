9801a0c8484b7082e4542cdcaa207f7252fd92ae
Marcos Amorim <markstm10@gmail.com>
2021-08-12 22:04:49 -0300

    🛠 criado rota para metodo post e get das transactions

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,7 @@ import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import { FormEvent, useState } from "react";
+import { api } from "../../services/api";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -22,7 +23,14 @@ export function NewTransactionModal({
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    console.log({ title, value, type, category });
+    const data = {
+      title,
+      value,
+      type,
+      category,
+    };
+
+    api.post("/transactions", data);
   }
 
   return (

c6c99869d748ed2ed639ef128af9bc5a3532190c
Marcos Amorim <markstm10@gmail.com>
2021-08-23 01:28:54 -0300

    🛠 somado valores totais do resumo com reduce. Formatado valores com a formatação de moeda

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -8,7 +8,28 @@ import { Container } from "./styles";
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
 
-  console.log(transactions);
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      switch (transaction.type) {
+        case "deposit":
+          acc.deposits += transaction.amount;
+          acc.total += transaction.amount;
+          break;
+        case "withdraw":
+          acc.withdraws += transaction.amount;
+          acc.total -= transaction.amount;
+          break;
+      }
+      return acc;
+    },
+    {
+      deposits: 0,
+      withdraws: 0,
+      total: 0,
+    }
+  );
+
+  console.log("total: ", summary);
   return (
     <Container>
       <div>
@@ -16,21 +37,37 @@ export function Summary() {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas" />
         </header>
-        <strong>R$1000,00</strong>
+        <strong>
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(summary.deposits)}
+        </strong>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={outcomeImg} alt="Saídas" />
         </header>
-        <strong>- R$500,00</strong>
+        <strong>
+          -
+          {new Intl.NumberFormat("pt-br", {
+            style: "currency",
+            currency: "brl",
+          }).format(summary.withdraws)}
+        </strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>R$ 500,00</strong>
+        <strong>
+          {new Intl.NumberFormat("pt-br", {
+            style: "currency",
+            currency: "brl",
+          }).format(summary.total)}
+        </strong>
       </div>
     </Container>
   );
